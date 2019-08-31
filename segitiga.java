18c456cd3eddd72dc50276a01d1e94a7b6fbc553
MuhammadRofiulHaq <54736935+rafielhaq@users.noreply.github.com>
2019-08-31 21:01:44 +0800

    Create segitiga.java

---
+++ segitiga.java
@@ -0,0 +1,14 @@
+public static void main (string[]args){
+  int i;
+  int j;
+  for(i=2;i<11;i++)
+     booelan isprima = true
+     for(j=2;j<1;j++){
+         if(i % j = 0){
+             isprima = false;
+              break;
+          }
+      }
+      if (isprima) {
+          system.out.print(i+ " ");
+          }
